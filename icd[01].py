1a621b1c5601afd5f7003a8671d231f708820120
Pedro Bastos <pedro.bastos@ctec.ufal.br>
2018-08-06 00:06:24 -0300

    bons avancos

--- icd[01].py
+++ icd[01].py
@@ -13,13 +13,32 @@ por exemplo, isso precisa ser revisto.
 """
 
 import pandas as pd
-from plotly.offline import plot
-import plotly.graph_objs as gr
+#from plotly.offline import plot
+#import plotly.graph_objs as gr
 
 mydataframe = pd.read_csv('VAZOES.ZIP', compression='zip', sep=';', 
                           encoding='ISO-8859-1', skiprows=16, parse_dates=['Data'], 
                           dayfirst=True, index_col='Data', decimal = ',' )
 
+#dates = pd.date_range(start='1995-01-01', end='2015-08-01')
+
+
+aux = mydataframe[['Vazao01', 'Vazao02', 'Vazao03', 'Vazao04', 'Vazao05', 'Vazao06',
+                   'Vazao07', 'Vazao08', 'Vazao09', 'Vazao10', 'Vazao11', 'Vazao12',
+                   'Vazao13', 'Vazao14', 'Vazao15', 'Vazao16', 'Vazao17', 'Vazao18',
+                   'Vazao19', 'Vazao20', 'Vazao21', 'Vazao22', 'Vazao23', 'Vazao24',
+                   'Vazao25', 'Vazao26', 'Vazao27', 'Vazao28', 'Vazao29', 'Vazao30', 'Vazao31']]
+#aux = pd.melt(aux)
+
+#test = aux.to_dict()
+
+newdataframe = pd.DataFrame()
+for x in range (0, 242):
+    test = [newdataframe, aux.iloc[x]]
+    newdataframe = pd.concat(test, ignore_index=True)
+#newdataframe = newdataframe.set_index(dates)      
+
+"""
 yearmean = mydataframe.groupby([pd.Grouper(freq='Y')]).mean()
 
 trace1 = gr.Scatter(
@@ -35,7 +54,7 @@ trace2 = gr.Scatter(
 
 data = [trace1, trace2]
 plot(data, filename='test')
-
+"""
 
 """
 Tentar uma abordagem diferente, gerar uma nova série a partir das medições diárias,
